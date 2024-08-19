cebb251880f516478c4842a44f712af32122ec4c
Basil Akavity <vakavity@gmail.com>
2024-08-19 11:04:27 +0300

    added WBTest and testNG

---
+++ src/test/java/org/akavity/WBTest.java
@@ -0,0 +1,21 @@
+package org.akavity;
+
+import org.akavity.steps.CatalogSteps;
+import org.akavity.steps.TabBarSteps;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WBTest extends BaseTest {
+
+    TabBarSteps tabBarSteps = new TabBarSteps();
+    CatalogSteps catalogSteps = new CatalogSteps();
+
+    @Test
+    public void moveThroughCatalog() {
+        tabBarSteps.clickCatalogButton();
+        catalogSteps.clickSectionButton("Женщинам");
+        catalogSteps.clickSubsectionButton("Брюки");
+
+        Assert.assertTrue(catalogSteps.extractTextFromTitle().equals("Брюки"));
+    }
+}
