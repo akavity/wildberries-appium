674883e52bfa1158ce123094ae0822ca95a63440
Basil Akavity <vakavity@gmail.com>
2025-02-25 17:33:47 +0300

    added new Locators

--- src/main/java/org/akavity/pages/CatalogPage.java
+++ src/main/java/org/akavity/pages/CatalogPage.java
@@ -2,6 +2,7 @@ package org.akavity.pages;
 
 import com.codeborne.selenide.appium.SelenideAppiumElement;
 import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.appium.SelenideAppium.$;
 
@@ -14,6 +15,14 @@ public class CatalogPage {
 
     public SelenideAppiumElement getSubsectionButton(String text) {
         return $(AppiumBy.xpath("//android.view.View[@resource-id='" + text + "']"));
+    }
+
+    public SelenideAppiumElement getTrend(String name) {
+        return $(By.xpath("//android.widget.TextView[@text='Тренд']/..//android.view.View[@content-desc='" + name + "']/.."));
+    }
+
+    public SelenideAppiumElement getTrendTitle(String title) {
+        return $(AppiumBy.xpath("//android.widget.TextView[@text='" + title + "']"));
     }
 
     public SelenideAppiumElement getTitleField() {
