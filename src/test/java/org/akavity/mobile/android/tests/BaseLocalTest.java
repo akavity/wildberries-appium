1359d29d7d0ba6456e88e4cc4ce7f9b249bb3f28
Basil Akavity <vakavity@gmail.com>
2025-03-05 17:39:14 +0300

    updated BaseLocalTest

--- src/test/java/org/akavity/mobile/android/tests/BaseLocalTest.java
+++ src/test/java/org/akavity/mobile/android/tests/BaseLocalTest.java
@@ -4,16 +4,19 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.appium.SelenideAppium;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.akavity.mobile.android.driver.LocalAndroidDriverWithApp;
+import org.akavity.mobile.android.driver.ci.CiAndroidDriverWithApp;
+import org.akavity.mobile.android.driver.local.LocalAndroidDriverWithApp;
 import org.testng.annotations.BeforeMethod;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static org.akavity.mobile.android.driver.AppiumUtils.isCi;
 
 public class BaseLocalTest {   //  extends ITTest
     @BeforeMethod
     public static void setup() {
         closeWebDriver();
-        Configuration.browser =  LocalAndroidDriverWithApp.class.getName();
+        Configuration.browser = isCi() ? CiAndroidDriverWithApp.class.getName() : LocalAndroidDriverWithApp.class.getName();
+        //Configuration.browser =  LocalAndroidDriverWithApp.class.getName();
         SelenideAppium.launchApp();
         SelenideLogger.addListener("allure", new AllureSelenide().screenshots(true));
     }
