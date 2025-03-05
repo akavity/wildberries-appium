66ab0f3672d28210486644536dbaf00a95f5e52b
Basil Akavity <vakavity@gmail.com>
2025-03-05 17:37:44 +0300

    added AndroidDriverProvider

---
+++ src/test/java/org/akavity/mobile/android/driver/ci/AndroidDriverProvider.java
@@ -0,0 +1,35 @@
+package org.akavity.mobile.android.driver.ci;
+
+import com.codeborne.selenide.WebDriverProvider;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
+import org.jspecify.annotations.NonNull;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.SessionNotCreatedException;
+import org.openqa.selenium.WebDriver;
+
+import static org.akavity.mobile.android.driver.AppiumUtils.*;
+
+public abstract class AndroidDriverProvider implements WebDriverProvider {
+    @Override
+    @NonNull
+    public WebDriver createDriver(@NonNull Capabilities capabilities) {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+        options.setPlatformName("Android");
+        options.setDeviceName("Pixel 8 Pro");
+        options.setPlatformVersion("14.0");
+        options.setCapability("bstack:options", getBrowserstackOptions());
+        options.setCapability("appium:settings", getAppiumSettings());
+        options.setApp(getApplicationUnderTest());
+        try {
+            return new AndroidDriver(browserstackUrl(), options);
+        } catch (SessionNotCreatedException e) {
+            return new AndroidDriver(browserstackUrl(), options);
+        }
+    }
+
+    protected abstract String getApplicationUnderTest();
+
+}
